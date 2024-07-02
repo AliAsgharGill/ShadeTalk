203bd5f4014c151da2d0c761cca9f4956faae9ef
AliAsgharGill <stylinalivlogs@gmail.com>
2024-07-02 13:28:17 +0500

    Signup User Complete and OTP

--- shade-talk/src/helpers/sendVerificationEmail.tsx
+++ shade-talk/src/helpers/sendVerificationEmail.tsx
@@ -1,26 +1,22 @@
-import { resend } from "resend";
+import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
-import { ApiResponse } from "@/types/ApiResponse";
+import { ApiResponse } from '@/types/ApiResponse';
 
 export async function sendVerificationEmail(
   email: string,
   username: string,
   verifyCode: string
 ): Promise<ApiResponse> {
-  const response = await resend;
   try {
     await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: 'dev@hiteshchoudhary.com',
       to: email,
-      subject: "Verification Code | ShadeTalk ",
+      subject: 'Mystery Message Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
     });
-    return { success: true, message: "Verification email sent successfully" };
+    return { success: true, message: 'Verification email sent successfully.' };
   } catch (emailError) {
-    console.log("Error sending verification email:");
-    return {
-      success: false,
-      message: "Failed to send verification email",
-    };
+    console.error('Error sending verification email:', emailError);
+    return { success: false, message: 'Failed to send verification email.' };
   }
 }
