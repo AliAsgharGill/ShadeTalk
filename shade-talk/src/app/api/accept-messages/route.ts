f1c0abd9927d512e77dc0f8bc82a56cc7c99a06b
AliAsgharGill <stylinalivlogs@gmail.com>
2024-07-02 21:50:10 +0500

    Post req of Message API

---
+++ shade-talk/src/app/api/accept-messages/route.ts
@@ -0,0 +1,60 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if (!session || session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "User not Authenticated",
+      },
+      { status: 404 }
+    );
+  }
+
+  const userId = user._id;
+  const { acceptMessages } = await request.json();
+
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      { isAcceptingMessage: acceptMessages },
+      {
+        new: true,
+      }
+    );
+    if (!updatedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 404 }
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message: "Messages accepted successfully",
+        updatedUser,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error accepting messages:", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error accepting messages",
+      },
+      { status: 500 }
+    );
+  }
+}
