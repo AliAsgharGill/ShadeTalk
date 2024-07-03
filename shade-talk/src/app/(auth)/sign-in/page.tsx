174827261fff52efd604ff363e360b0fc5b14567
AliAsgharGill <stylinalivlogs@gmail.com>
2024-07-03 19:59:37 +0500

    React hook form, shadcn and debouncing

--- shade-talk/src/app/(auth)/sign-in/page.tsx
+++ shade-talk/src/app/(auth)/sign-in/page.tsx
@@ -1,39 +1,11 @@
-"use client";
-import { useSession, signIn, signOut } from "next-auth/react";
-import Image from "next/image";
+import React from 'react'
 
-export default function Component() {
-  const { data: session } = useSession();
-
-  if (session) {
-    return (
-      <>
-        <div className="flex justify-around items-center space-x-3 w-1/4 p-5 ">
-          <Image
-            className="rounded-full"
-            priority={false}
-            src={session.user?.image ?? "No Image"}
-            alt="Description of image"
-            width={50}
-            height={50}
-          />
-          Signed in as {session.user?.email ?? "unknown"} <br />
-          <div>
-            <button onClick={() => signOut()}>Sign out</button>
-          </div>
-        </div>
-      </>
-    );
-  }
+const Singin = () => {
   return (
-    <>
-      <div className="flex justify-around items-center space-x-3 w-1/4 p-5 ">
-        Not signed in <br />
-        <button onClick={() => signIn()}>Sign in</button>
-        {/* if we want to login directly by click on button then write name of provider e.g. google, github in signIn() as parameter */}
-        {/* <button onClick={() => signIn("google")}>Sign in with Google</button>
-      <button onClick={() => signIn("github")}>Sign in with Github</button> */}
-      </div>
-    </>
-  );
+    <div>
+      Sign In
+    </div>
+  )
 }
+
+export default Singin
--- shade-talk/src/app/(auth)/sign-up/page.tsx
+++ shade-talk/src/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,232 @@
-import React from "react";
-
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import Link from "next/link";
+import { useDebounceValue, useDebounceCallback } from "usehooks-ts";
+import React, { useEffect, useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 const Signup = () => {
-  return <div>Signup Page</div>;
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const debounced = useDebounceCallback(setUsername, 500);
+  const { toast } = useToast();
+  const route = useRouter();
+
+  // zod implementation
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      if (username) {
+        setIsCheckingUsername(true);
+        setUsernameMessage("");
+        try {
+          const response = await axios.get(
+            `/api/check-username-unique?username=${username}`
+          );
+          setUsernameMessage(response.data.message);
+          // we can also set using following method
+          // let message = response.data.message;
+          // setUsernameMessage(message);
+        } catch (error) {
+          console.log("Error", error);
+          const axiosError = error as AxiosError<ApiResponse>;
+          setUsernameMessage(
+            axiosError.response?.data.message ?? "Error checking username"
+          );
+        } finally {
+          setIsCheckingUsername(false);
+        }
+      }
+    };
+    checkUsernameUnique();
+  }, [username]);
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true);
+    try {
+      const response = await axios.post<ApiResponse>("api/sign-up", data);
+      toast({
+        title: "Success",
+        description: response.data.message,
+      });
+      route.replace(`/verify/${username}`);
+      setIsSubmitting(false);
+    } catch (error) {
+      console.log("Error in signup for user", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = axiosError.response?.data.message;
+      toast({
+        title: "Error",
+        description: axiosError.response?.data.message,
+      });
+    }
+
+    setIsSubmitting(false);
+  };
+
+  const handleToast = () => {
+    toast({
+      title: "Scheduled: Catch up",
+      description: "Friday, February 10, 2023 at 5:57 PM",
+    });
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      {/* <button onClick={handleToast}>Toast</button> */}
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-xl flex justify-center font-extrabold tracking-tight lg:text-5xl mb-6 ">
+          Join ShadeTalk
+        </div>
+        <p className="mb-t flex justify-center ">
+          Sign up to start your anonymous adventure
+        </p>
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="username "
+                        {...field}
+                        onChange={(e) => {
+                          field.onChange(e);
+                          debounced(e.target.value);
+                        }}
+                      />
+                    </FormControl>
+                    {isCheckingUsername && (
+                      <Loader2 className="w-4 h-4 animate-spin" />
+                    )}
+                    <p
+                      className={`text-sm ${
+                        usernameMessage === "Username is available"
+                          ? "text-green-500"
+                          : "text-red-500"
+                      } `}
+                    >
+                      {usernameMessage}
+                    </p>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Email */}
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="example@domain.com"
+                        {...field}
+                        onChange={(e) => {
+                          field.onChange(e.target.value);
+                        }}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Please write personal email address.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* Passowrd */}
+              <FormField
+                name="password"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="******"
+                        {...field}
+                        onChange={(e) => {
+                          field.onChange(e.target.value);
+                        }}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Please write storng password.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                variant="default"
+                className="w-full"
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? (
+                  <>
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                    Loading...
+                  </>
+                ) : (
+                  "Sign Up"
+                )}
+              </Button>
+            </form>
+          </Form>
+          <div className="text-center mt-4">
+            <p>
+              Already have an account?{" "}
+              <Link
+                href={"/sign-in"}
+                className="text-blue-600 hover:text-blue-800"
+              >
+                Sign in
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
